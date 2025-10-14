4f5ba8bad76b0e8a777e8d03ddacc22adebb36fe
让你飞起来 <2171954760@qq.com>
2025-10-14 14:36:52 +0800

    1

---
+++ academic-sharing-platform/src/test/java/com/team13/academic_sharing_platform/AcademicSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team13.academic_sharing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
